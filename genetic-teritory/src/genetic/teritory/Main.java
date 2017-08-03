18021e99188b24a45666f66f2067aaebc41d1faa
diegodifreitas <diegodifreitas@gmail.com>
2017-08-03 00:06:08 -0300

    refactoring main class

--- genetic-teritory/src/genetic/teritory/Main.java
+++ genetic-teritory/src/genetic/teritory/Main.java
@@ -24,40 +24,47 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //PA  CM  SR  IB
-        int[][] mapa = {{0, 1, 1, 0}, //PA
-        {1, 0, 1, 0}, //CM
-        {1, 1, 0, 1}, //SR
-        {0, 0, 1, 0}}; //IB
-
         Main obj = new Main();
         obj.execute();
     }
 
     private void execute() {
+        Mapa map = iniciaDados();
+        //TODO LOGIC FOR ALTER COLOR
+        saida(map);
+    }
 
-        List<Territorio> territorioTemp = new ArrayList<>();
-        
-        Territorio srs = new Territorio();        
+    public void saida(Mapa map) {
+        System.out.println("Mapa: " + map.getId());
+        map.getTerritorios()
+                .stream()
+                .forEach((item) -> {
+                    System.out.println("Teritorio: " + item.getId());
+                    System.out.println("Cor: " + item.getCor());
+                });
+    }
+
+    private Mapa iniciaDados() {
+        Territorio srs = new Territorio();
         Territorio pa = new Territorio();
         Territorio cm = new Territorio();
         Territorio itj = new Territorio();
-        
+
         srs.addAdj(cm);
         srs.addAdj(itj);
         srs.addAdj(pa);
-        
+
         pa.addAdj(cm);
         pa.addAdj(srs);
-        
+
         itj.addAdj(srs);
-        
+
         Mapa map = new Mapa();
         map.addTerritorio(cm);
         map.addTerritorio(pa);
         map.addTerritorio(srs);
         map.addTerritorio(itj);
-        
+        return map;
     }
 
 }
